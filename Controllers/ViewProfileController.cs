69f8d1b33895eba89560e93d4e5a53af5552988c
Nguyễn Tuấn Anh HE171551 <anhnthe171551@fpt.edu.vn>
2025-03-27 21:44:23 +0700

    Tuan Huy Commit

---
+++ Controllers/ViewProfileController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+public class ViewProfileController : Controller
+{
+    private readonly ClothingShopDbContext _context;
+    
+
+    public ViewProfileController(ClothingShopDbContext context)
+    {
+        _context = context;
+        
+    }
+
+    // GET: /ViewProfile/View/1
+    public IActionResult View(int id)
+    {
+        var user = _context.Users.FirstOrDefault(u => u.UserId == id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return View(user);
+    }
+
+    // GET: /ViewProfile/Edit/1
+    public IActionResult Edit(int id)
+    {
+        var user = _context.Users.FirstOrDefault(u => u.UserId == id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return View(user);
+    }
+
+    // POST: /ViewProfile/Edit
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(int UserId, string FullName, string Email, string Phone, string Address)
+    {
+        
+
+        // Find the user
+        var user = _context.Users.Find(UserId);
+
+      
+
+        try
+        {
+            // Update user properties
+            user.FullName = FullName;
+            user.Email = Email;
+            user.Phone = Phone;
+            user.Address = Address;
+
+            _context.SaveChanges();
+            
+
+            // Redirect to view profile
+            return RedirectToAction("View", new { id = UserId });
+        }
+        catch (Exception ex)
+        {
+           
+
+            // Re-create the user object to send back to the view
+            var userViewModel = new User
+            {
+                UserId = UserId,
+                FullName = FullName,
+                Email = Email,
+                Phone = Phone,
+                Address = Address
+            };
+
+            ModelState.AddModelError("", "Unable to save changes: " + ex.Message);
+            return View(userViewModel);
+        }
+    }
+
+    public IActionResult Settings()
+    {
+        return View();
+    }
+}
